1f84f54d8a38cb37dffbc2bbbe9495082945427a
Lucas Genvigir <lucasgenvigir@alunos.utfpr.edu.br>
2026-02-08 19:21:37 -0300

    Melhorando a movimentação da bola

--- sketch.js
+++ sketch.js
@@ -1,21 +1,42 @@
-// posicao da bola
-var x = 200;
-var y = 200;
+class Bola {
+    constructor() {
+        this.r = 25;
+        this.reset();
+    }
 
-// velociadades da bola
-var vx = 5;
-var vy = 5;
+    reset() {
+        this.x = width / 2;
+        this.y = height / 2;
+        this.vx = Math.random() * 10 - 5;
+        this.vy = Math.random() * 10 - 5;
+    }
 
-//funcao setup do p5js
-function setup() {
-    createCanvas(400, 400);
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+        if (this.x < this.r || this.x > width - this.r) {
+            this.reset();
+        }
+        if (this.y < this.r || this.y > height - this.r) {
+            this.vy *= -1;
+        }
+    }
+
+    desenha() {
+        fill(color(255, 0, 0))
+        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+    }
 }
 
-//funcao de desenho do p5js
+let bola;
+
+function setup() {
+    createCanvas(800, 400);
+    bola = new Bola();
+}
+
 function draw() {
-    background(0);
-    fill(255);
-    ellipse(x, y, 50, 50);
-    x += vx;
-    y += vy;
+    background(color(0, 0, 0));
+    bola.update();
+    bola.desenha();
 }
