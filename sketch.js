17833905ab2d87e672b77fa97e750ef6974e7827
Lucas Genvigir <lucasgenvigir@alunos.utfpr.edu.br>
2026-02-08 19:39:56 -0300

    Colisão entre raquete e bola

--- sketch.js
+++ sketch.js
@@ -1,3 +1,23 @@
+class Raquete {
+    constructor(x) {
+        this.x = x;
+        this.y = height / 2;
+        this.w = 10;
+        this.h = 100; 
+    }        
+
+    update() {
+        this.y = mouseY;
+        // limitar dentro da tela
+        this.y = constrain(this.y, this.h / 2, height - this.h / 2);   
+    }
+
+    desenha() { 
+        fill(color(255, 255, 255))
+        rect(this.x, this.y - this.h / 2, this.w, this.h);
+    }
+}
+
 class Bola {
     constructor() {
         this.r = 25;
@@ -20,6 +40,14 @@ class Bola {
         if (this.y < this.r || this.y > height - this.r) {
             this.vy *= -1;
         }
+
+        // se colidir com a raquete
+        if (this.x - this.r < raquete.x + raquete.w && 
+            this.y > raquete.y - raquete.h / 2 && 
+            this.y < raquete.y + raquete.h / 2) {
+            this.vx *= -1;
+            this.x = raquete.x + raquete.w + this.r; // evitar ficar preso na raquete
+        }
     }
 
     desenha() {
@@ -29,14 +57,18 @@ class Bola {
 }
 
 let bola;
+let raquete;
 
 function setup() {
     createCanvas(800, 400);
     bola = new Bola();
+    raquete = new Raquete(50);
 }
 
 function draw() {
     background(color(0, 0, 0));
     bola.update();
     bola.desenha();
+    raquete.update();
+    raquete.desenha();
 }
