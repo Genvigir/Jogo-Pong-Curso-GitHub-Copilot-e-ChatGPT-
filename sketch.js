06384589931f6ef2007fa008554c9bf400906157
Lucas Genvigir <lucasgenvigir@alunos.utfpr.edu.br>
2026-02-09 19:52:43 -0300

    Adicionando Imagens ao jogo e giro da bola

--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+let bolaImagem;
+let raqueteImagem;
+let raqueteOponenteImagem;
+let fundoImagem;    
+
 class Raquete {
     constructor(x) {
         this.x = x;
@@ -26,8 +31,11 @@ class Raquete {
     }
 
     desenha() { 
-        fill(color(255, 255, 255))
-        rect(this.x, this.y - this.h / 2, this.w, this.h);
+        if (this.x < width / 2) {
+            image(raqueteImagem, this.x, this.y - this.h / 2, this.w, this.h);
+        } else {
+            image(raqueteOponenteImagem, this.x, this.y - this.h / 2, this.w, this.h);
+        }
     }
 }
 
@@ -35,6 +43,8 @@ class Bola {
     constructor() {
         this.r = 25;
         this.reset();
+        // angulo rotacao atual
+        this.angle = 0;
     }
 
     reset() {
@@ -45,6 +55,7 @@ class Bola {
     }
 
     update() {
+        this.angle += 0.1;  
         this.x += this.vx;
         this.y += this.vy;
         if (this.x < this.r || this.x > width - this.r) {
@@ -76,14 +87,24 @@ class Bola {
     }
 
     desenha() {
-        fill(color(255, 0, 0))
-        ellipse(this.x, this.y, this.r * 2, this.r * 2);
+        push();
+        translate(this.x, this.y);
+        rotate(this.angle);
+        image(bolaImagem, -this.r, -this.r, this.r * 2, this.r * 2);
+        pop();
     }
 }
 
 let bola;
 let raquete;
 let raqueteOponente;
+
+function preload() {
+    bolaImagem = loadImage('bola.png');
+    raqueteImagem = loadImage('barra01.png');
+    raqueteOponenteImagem = loadImage('barra02.png');
+    fundoImagem = loadImage('fundo2.png');
+}
 
 function setup() {
     createCanvas(800, 400);
@@ -92,118 +113,24 @@ function setup() {
     raqueteOponente = new Raquete(width - 50);
 }
 
-function draw() {class Raquete {
-    constructor(x) {
-        this.x = x;
-        this.y = height / 2;
-        this.w = 10;
-        this.h = 60;
-    }
-    update() {
-
-        // se a raquete é o jogador
-        if (this.x < width / 2) {
-            this.y = mouseY;
-        } else {
-            // se a bola está em cima vai pra cima
-            if (bola.y < this.y) {
-                this.y -= 5;
-            } else {
-                // se a bola está em baixo vai pra baixo
-                this.y += 5;
-            }
-        }
-
-        //limitar dentro da tela
-        if (this.y < 0) {
-            this.y = 0;
-        }
-        if (this.y > height - this.h) {
-            this.y = height - this.h;
-        }
-    }
-    desenha() {
-        fill(color(255, 255, 255));
-        rect(this.x, this.y, this.w, this.h);
-    }
-}
-
-class Bola {
-    constructor() {
-        this.r = 25;
-        this.reset();
-    }
-
-    reset() {
-        this.x = width / 2;
-        this.y = height / 2;
-        const velecidadeMaxima = 5
-        this.vx = Math.random() * velecidadeMaxima * 2 - velecidadeMaxima;
-        this.vy = Math.random() * velecidadeMaxima * 2 - velecidadeMaxima;
-    }
-
-    update() {
-        this.x += this.vx;
-        this.y += this.vy;
-        
-        if (this.x < this.r || this.x > width - this.r) {
-            this.reset();
-        }
-        if (this.y < this.r || this.y > height - this.r) {
-            this.vy *= -1;
-        }
-
-        if (colideRetanguloCirculo(this.x, this.y, this.r, jogador.x, jogador.y, jogador.w, jogador.h) ||
-            colideRetanguloCirculo(this.x, this.y, this.r, computador.x, computador.y, computador.w, computador.h)) {
-            this.vx *= -1;
-            this.vx *= 1.1;
-            this.vy *= 1.1;
-        }
-
-    }
-
-    desenha() {
-        fill(color(255, 0, 0))
-        ellipse(this.x, this.y, this.r * 2, this.r * 2);
-    }
-}
-
-// verifica a colisão entre um círculo e retângulo
-// onde círculo é raio e cx, cy
-// e retângulo é x, y, w, h
-function colideRetanguloCirculo(cx, cy, raio, x, y, w, h) {
-    // se o círculo está a esquerda ou a direita do retângulo
-    if (cx + raio < x || cx - raio > x + w) {
-        return false;
-    }
-    // se o círculo está acima ou abaixo do retângulo
-    if (cy + raio < y || cy - raio > y + h) {
-        return false;
-    }
-    return true;
-}
-
-let bola;
-let jogador;
-let computador;
-
-function setup() {
-    createCanvas(800, 400);
-    bola = new Bola();
-    jogador = new Raquete(30);
-    computador = new Raquete(width - 30 - 10);
-}
-
 function draw() {
-    background(color(0, 0, 0));
-    bola.update();
-    bola.desenha();
-    jogador.update();
-    jogador.desenha();
-    computador.update();
-    computador.desenha();
-}
-    background(color(0, 0, 0));
+
+    // desenha centralizada o fundoImagem, com o aspectRatio do canvas, 
+    // e zomm out o máximo possivel
+// centralized fundoImagem, with canvas aspectRatio, and zoom out as maximun as possible
+    let canvasAspectRatio = width / height;
+    let fundoAspectRatio = fundoImagem.width / fundoImagem.height;
+    let zoom = 1;
+    if (canvasAspectRatio > fundoAspectRatio) {
+        zoom = width / fundoImagem.width;
+    } else {
+        zoom = height / fundoImagem.height;
+    }
+    let scaledWidth = fundoImagem.width * zoom;
+    let scaledHeight = fundoImagem.height * zoom;
+    image(fundoImagem, (width - scaledWidth) / 2, (height - scaledHeight) / 2, scaledWidth, scaledHeight);
+
+
     bola.update();
     bola.desenha();
     raquete.update();
